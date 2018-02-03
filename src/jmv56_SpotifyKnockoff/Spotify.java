8436408f5daac03c7ae141ea67694405fe84fe98
Zendiagram <35267111+Zendiagram@users.noreply.github.com>
2018-02-02 19:24:25 -0500

    Fixing the problems

---
+++ src/jmv56_SpotifyKnockoff/Spotify.java
@@ -0,0 +1,43 @@
+package JMV56_SpotifyKnockoff;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Spotify {
+
+	public static void main(String[] args) {
+		String hostName = "sis-teach-01.sis.pitt.edu";
+		String dbName = "spotify_knockoff";
+		String userName = "spotifyUser";
+		String passWord = "spotifyUser123";
+		
+		String connString = "jdbc:mysql://" + hostName + "/" + dbName + "?user=" + userName + "&password=" + passWord;
+		
+		System.out.println(connString);
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Connection conn = DriverManager.getConnection(connString);
+
+			String sql = "SELECT * FROM users;";
+			Statement statement = conn.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			
+			while(result.next()) {
+				System.out.println(result.getString(1) + "\t" + result.getString(2));
+			}
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
