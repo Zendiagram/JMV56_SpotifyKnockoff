342efb9bbffde2c4ae6a6b202e96b70904a74757
Zendiagram <35267111+Zendiagram@users.noreply.github.com>
2018-02-12 11:56:40 -0500

    updating the Search Spotify class

---
+++ src/jmv56_SpotifyKnockoff/SearchSpotify.java
@@ -0,0 +1,65 @@
+package JMV56_SpotifyKnockoff;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class SearchSpotify {
+	
+	private static DefaultTableModel searchSongs(String searchTerm) {
+		String sql = "SELECT song_id, title, length, release_date, record_date FROM song ";
+		
+		String [] columns = {"Song ID","Title","Length","Release Date","Record Date"};
+		
+		if(!searchTerm.equals("")) {
+			sql += "WHERE title LIKE '%" + searchTerm + "%';";
+		}
+			try {
+				DbUtilities db = new DbUtilities();
+				return db.getDataTable(sql, columns);
+				
+			} catch (SQLException e) {
+				JOptionPane.showMessageDialog(frame, "Unable to connect to database");
+				ErrorLogger.log(e.getMessage());
+			}
+			return null;	 		
+	}
+	
+	private static DefaultTableModel searchAlbums(String searchTerm) {
+		String sql = "SELECT album_id, title, release_date, recording_company_name, number_of_tracks, PMRC_rating, length FROM album ";
+		String [] columns = {"Album ID","Title","Release Date","Recording Company Name","Number of Tracks", "PMRC Rating", "Length"};
+		
+		if(!searchTerm.equals("")) {
+			sql += "WHERE title LIKE '%" + searchTerm + "%';";
+		}
+		
+		try {
+			DbUtilities db = new DbUtilities();
+			return db.getDataTable(sql, columns);
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(frame, "Unable to connect to database");		
+			ErrorLogger.log(e.getMessage());		
+		}
+		return null;
+	}
+	
+	private static DefaultTableModel searchArtist(String searchTerm) {
+		String sql = "SELECT artist_id, first_name, last_name, band_name, bio FROM artist ";
+		
+		if(!searchTerm.equals("")) {
+			sql += "WHERE last_name LIKE '%" + searchTerm + "%' OR first_name LIKE '%" + searchTerm + "%' OR band_name LIKE '%" + searchTerm + "%';";
+		}
+		try {
+			DbUtilities db = new DbUtilities();
+			String [] columns = {"Artist ID","First Name","Last Name","Band Name","Bio"};
+			return db.getDataTable(sql, columns);
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(frame, "Unable to connect to database");
+			ErrorLogger.log(e.getMessage());
+		}
+		return null;
+	}
+}
